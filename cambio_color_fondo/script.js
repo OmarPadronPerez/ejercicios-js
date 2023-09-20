a1cbe359578d453acb30c9b67d31c12982a6e26c
javier padron <omar.padron.perez@gmail.com>
2023-09-20 16:55:39 -0600

    fix cambio de color

--- cambio_color_fondo/script.js
+++ cambio_color_fondo/script.js
@@ -1,21 +1,24 @@
 console.log("en JS");
 
-function cambioColor() {
-    let color="#"+colorAleatorio();
-    console.log(color);
-    document.getElementById("colorFondo").innerHTML=color;
-    document.getElementById("body").style.backgroundColor=color;
-
+function colorAleatorio() {
+    cambiarFondo(seleccioncolor());
 }
 
-function colorAleatorio() {
+function seleccioncolor() {
     var letras = ["a", "b", "c", "d", "e", "f", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    
     let color="";
     for(let i=0;i<6;i++){
         let numero = (Math.random() * 15).toFixed(0);
         color+=letras[numero]
     }
     return color;
+}
 
+function cambiarFondo(color){
+    if(color.charAt(0)!='#'){
+        console.log(color);
+        color="#"+color;
+    }
+    document.getElementById("colorFondo").innerHTML=color;
+    document.getElementById("body").style.backgroundColor=color;
 }
